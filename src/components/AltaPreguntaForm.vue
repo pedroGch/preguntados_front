3d42d9e0c0fd0ab1abcf5abc5e2591b92999f213
Pedro Gonzalez Chavez <pedro.gch01@gmail.com>
2022-01-19 20:40:57 -0300

    cambios

--- src/components/AltaPreguntaForm.vue
+++ src/components/AltaPreguntaForm.vue
@@ -3,7 +3,7 @@
     <div class="col s12 m8 l10 centrado">
       <div class="card-panel">
         <h4 class="header2">Crear Cuestionarios</h4>
-        <div class="row">
+        <div class="row  ml-13">
           <form class="col s12">
             <div class="row">
               <div class="input-field col s12 l12 m12">
@@ -17,29 +17,27 @@
               <div class="col s12 m2 l2 input-field"> <a class="modal-trigger btn cyan darken-1 texto-boton" href="#" @click="agregarRespuesta()" >Agregar<i class="material-icons right">add</i> </a> </div>
             </div>
             <div class="row">
-              <ul>
-                <li v-for="(pregunta, i ) in preguntas">
-                  <input class="col s12 l10 m10" type="text" v-model="pregunta.pregunta" :disabled="!pregunta.editable" placeholder="Agregar una respuesta"> 
-                
-                  <a class="btn-floating btn-flat  lighten-1 " :class="[pregunta.estado ? 'green': 'grey']" title="marcar como correcta" href="#" @click="marcarComoCorrecta(i)">
-                    <i class="material-icons"> done</i>
-                  </a>
-                  <a class="btn-floating btn-flat darken-1 " :class="[pregunta.editable ? 'orange' : 'blue']" :title="pregunta.editable ? 'Guardar edición' : 'editar respuesta'" href="#" @click="editarRespuesta(pregunta)">
-                    <i class="material-icons"> {{pregunta.editable ? 'save':'mode_edit'}}</i>
-                  </a>
-                  <a class="btn-floating btn-flat red darken-1 " title="eliminar pregunta" href="#" @click="eliminarRespuesta(i)">
-                    <i class="material-icons"> delete_forever</i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="row">
-              <div class="row">
-                <div class="input-field col s12">
-                  <button class="btn cyan waves-effect waves-light right" type="submit" name="action" @click="guardarCuentionario()" >Guardar
-                    <i class="material-icons right">send</i>
-                  </button>
-                </div>
+              <div class="col s12">
+                <ul>
+                  <li v-for="(pregunta, i ) in preguntas">
+                    <input class="col s12 l10 m10" type="text" v-model="pregunta.pregunta" :disabled="!pregunta.editable" placeholder="Agregar una respuesta"> 
+                  
+                    <a class="btn-floating btn-flat  lighten-1 " :class="[pregunta.estado ? 'green': 'grey']" title="marcar como correcta" href="#" @click="marcarComoCorrecta(i)">
+                      <i class="material-icons"> done</i>
+                    </a>
+                    <a class="btn-floating btn-flat darken-1 " :class="[pregunta.editable ? 'orange' : 'blue']" :title="pregunta.editable ? 'Guardar edición' : 'editar respuesta'" href="#" @click="editarRespuesta(pregunta)">
+                      <i class="material-icons"> {{pregunta.editable ? 'save':'mode_edit'}}</i>
+                    </a>
+                    <a class="btn-floating btn-flat red darken-1 " title="eliminar pregunta" href="#" @click="eliminarRespuesta(i)">
+                      <i class="material-icons"> delete_forever</i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="input-field col s12">
+                <button class="btn cyan waves-effect waves-light right" type="submit" name="action" @click="guardarCuentionario()" >Guardar
+                  <i class="material-icons right">send</i>
+                </button>
               </div>
             </div>
           </form>
@@ -51,7 +49,7 @@
 
 <script>
 
-
+import axios from 'axios'
 export default {
   name: 'AltaPreguntaForm',
   components:{
@@ -102,16 +100,18 @@ export default {
     },
     guardarCuentionario(){
       //validar que el campo pregunta tenga al menos un texto
-
       //validar que de las respuestas solo una sea verdadera
       let respuestaCorrecta =  this.preguntas.find(p => p.estado == true) 
-      let respuestaAEnviar = {
+      let enviarPregunta = {
         "nombre": this.pregunta_descrip,
         "materia": "IBD",
         "tema": "teoria 1",
         "respuesta" : respuestaCorrecta.pregunta
       }
-      console.log(respuestaAEnviar);
+      axios.post("localhost:5000/api/v1/preguntas/altaPregunta",enviarPregunta).then((result) => {
+        console.log(result)
+      })
+      //enviar por axios la pregunta
 
     }
   }
@@ -144,5 +144,11 @@ export default {
 
 .row form{
   width: 70% !important;
+}
+.ml-13{
+  margin-left: 20.5% !important;
+}
+.desabilitar{
+  filter: grayscale(100%);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
+
 Vue.use(VueMaterial)
 
 import "./app.css"
